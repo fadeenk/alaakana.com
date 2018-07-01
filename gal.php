04c040042a77f657ea19ea6069182e4f50c3894f
Fadee kannah <fadeekannah@gmail.com>
2018-06-30 20:10:44 -0700

    Add ACTFL pictures

--- gal.php
+++ gal.php
@@ -16,22 +16,26 @@
 <div id="temp_main">
 		<h2>Galleries</h2>
         <div class="gallery_box">
-        <a href="galleries/pix/coworkers/20180601_221112.jpg" rel="lightbox[portfolio]"><img src="galleries/pix/coworkers/20180601_221112.jpg" style="width:200px;"></a>
-        <a href="galleries/coworkers.php">Coworkers at Gaslamp Hilton Hotel</a>
+          <a href="galleries/pix/ACTFL/title.jpg" rel="lightbox[portfolio]"><img src="galleries/pix/ACTFL/title.jpg" style="width:200px;"></a>
+          <a href="galleries/ACTFL.php">Annual Convention and World Languages Expo (ACTFL)</a>
+        </div>
+        <div class="gallery_box">
+          <a href="galleries/pix/coworkers/20180601_221112.jpg" rel="lightbox[portfolio]"><img src="galleries/pix/coworkers/20180601_221112.jpg" style="width:200px;"></a>
+          <a href="galleries/coworkers.php">Coworkers at Gaslamp Hilton Hotel</a>
         </div>
 		    <div class="gallery_box">
        		<a href="galleries/pix/startalk/20170728_132121.jpg" rel="lightbox[portfolio]"><img src="galleries/pix/startalk/20170728_132121.jpg" style="width:200px;"></a>
        		<a href="galleries/startalk.php">STARTALK Program Tour at Balboa Park</a>
         </div>
-        <div class="gallery_box">
+        <div class="gallery_box no_margin_right">
           <a href="galleries/pix/Phd/photo_028.jpg" rel="lightbox[portfolio]"><img src="galleries/pix/Phd/photo_028.jpg" style="width:200px;"></a>
           <a href="galleries/phd.php">Ph.D. Pictures in Jordan</a>
         </div>
-        <div class="gallery_box no_margin_right">
+        <div class="cleaner"></div>
+        <div class="gallery_box">
             <a href="galleries/pix/baghdad hotel/photo_031.jpg" rel="lightbox[portfolio]"><img src="galleries/pix/baghdad hotel/photo_031.jpg" style="width:200px;"></a>
             <a href="galleries/b_hotel.php">Baghdad Hotel in Iraq</a>
         </div>
-        <div class="cleaner"></div>
         <div class="gallery_box">
             <a href="galleries/pix/october/photo_006.jpg" rel="lightbox[portfolio]"><img src="galleries/pix/october/photo_006.jpg" style="width:200px;"></a>
             <a href="galleries/october.php">October 6 Universtiy in Egypt</a>
@@ -40,15 +44,15 @@
             <a href="galleries/pix/ahlea/photo_036.jpg" rel="lightbox[portfolio]"><img src="galleries/pix/ahlea/photo_036.jpg" style="width:200px;"></a>
             <a href="galleries/ahlea.php">AL-Ahliyya Amman University in Jordan</a>
         </div>
-        <div class="gallery_box">
+        <div class="gallery_box no_margin_right">
        		<a href="galleries/pix/comic con/photo_008.jpg" rel="lightbox[portfolio]"><img src="galleries/pix/comic con/photo_008.jpg" style="width:200px;"></a>
           <a href="galleries/comic con.php">Comic-Con International in San Diego, California</a>
         </div>
-        <div class="gallery_box no_margin_right">
+        <div class="cleaner"></div>
+        <div class="gallery_box">
             <a href="galleries/pix/course/photo_007.jpg" rel="lightbox[portfolio]"><img src="galleries/pix/course/photo_007.jpg" style="width:200px;"></a>
             <a href="galleries/course.php">Hotel & Restaurant Management Course in Baghdad Hotel</a>
         </div>
-        <div class="cleaner"></div>
         <div class="gallery_box">
             <a href="galleries/pix/c amman/photo_003.jpg" rel="lightbox[portfolio]"><img src="galleries/pix/c amman/photo_003.jpg" style="width:200px;"></a>
             <a href="galleries/c amman.php">Restaurant Management Course in Imperial Palace Hotel in Jordan</a>
@@ -57,11 +61,12 @@
             <a href="galleries/pix/others/photo_003.jpg" rel="lightbox[portfolio]"><img src="galleries/pix/others/photo_003.jpg" style="width:200px;"></a>
             <a href="galleries/others.php">Miscellaneous</a>
         </div>        
-        <div class="gallery_box">
+        <div class="gallery_box no_margin_right">
             <a href="galleries/pix/h t/photo_003.jpg" rel="lightbox[portfolio]"><img src="galleries/pix/h t/photo_003.jpg" style="width:200px;"></a>
             <a href="galleries/h t.php">Hilton San Diego Gaslamp Quarter hotel</a>
         </div>
-        <div class="gallery_box no_margin_right">
+        <div class="cleaner"></div>
+        <div class="gallery_box">
             <a href="galleries/pix/tc/photo_003.jpg" rel="lightbox[portfolio]"><img src="galleries/pix/tc/photo_003.jpg" style="width:200px;"></a>
             <a href="galleries/tc.php">Practical Training Courses in Arena Hotel in Jordan</a>
         </div>
